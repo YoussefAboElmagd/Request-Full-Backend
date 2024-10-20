80b351d85e67c903f7c5e8ffc11165caceab4064
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-10-20 17:42:58 +0300

    team in project

--- src/modules/Project/project.controller.js
+++ src/modules/Project/project.controller.js
@@ -412,13 +412,17 @@ const getAllMembersProject = catchAsync(async (req, res, next) => {
     }));
   }
   
-  let roles = ["owner","consultant","contractor"];
-  const groupAdmins = members.reduce((acc, member) => {
-    if (roles.includes(member.role)) {
-      acc[member.role] = member;
-    }
+  let roles = ["owner", "consultant", "contractor"];
+  const groupAdmins = roles.reduce((acc, role) => {
+    acc[role] = null; // Initialize each role with null
     return acc;
   }, {});
+  
+  members.forEach(member => {
+    if (roles.includes(member.role)) {
+      groupAdmins[member.role] = member; // Update with member details if present
+    }
+  });
   let groupedMembers = []
   groupedMembers = members.reduce((acc, member) => {
     if (!roles.includes(member.role)) {      
