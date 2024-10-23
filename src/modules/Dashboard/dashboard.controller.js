129beacad11c66e8784e3d14e57afb9f6d02b748
Abdelrhmanm7md <abdelrahmanmohammed851@gmail.com>
2024-10-23 13:14:01 +0300

    Performance evaluation

--- src/modules/Dashboard/dashboard.controller.js
+++ src/modules/Dashboard/dashboard.controller.js
@@ -1,7 +1,9 @@
+import mongoose from "mongoose";
 import { projectModel } from "../../../database/models/project.model.js";
 import { userModel } from "../../../database/models/user.model.js";
 import ApiFeature from "../../utils/apiFeature.js";
 import catchAsync from "../../utils/middleWare/catchAsyncError.js";
+import { taskModel } from "../../../database/models/tasks.model.js";
 
 const getAllDashboard = catchAsync(async (req, res, next) => {
   let ApiFeat = new ApiFeature(DashboardCodeModel.find(), req.query).search();
@@ -140,6 +142,58 @@ const getActiveProjects= catchAsync(async (req, res, next) => {
     results,
   });
 });
+const getActiveProjectsByUser= catchAsync(async (req, res, next) => {
+  let id =new mongoose.Types.ObjectId(req.params.id)
+  
+let check = await userModel.findById(id);
+if (!check) { 
+  return res.status(404).json({ message: "User not found!" });
+}
+  let ApiFeat = new ApiFeature(
+    projectModel.aggregate([
+      {
+        $match: 
+          { $and: [{ members:  id  }, { status: "working" }] } 
+      },
+      {
+        $lookup: {
+          from: "users", // Assuming the 'members' refer to the user model
+          localField: "members",
+          foreignField: "_id",
+          as: "members"
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          members: {
+            _id: 1,
+            name: 1,
+            profilePic: 1
+          },
+          progress: 1,
+          tasksCount: { $size: "$tasks" } // Count the number of tasks
+        }
+      }
+    ]),
+    req.query
+  )
+    .sort()
+    .search();
+
+  let results = await ApiFeat.mongooseQuery;
+
+  if (!ApiFeat || !results) {
+    return res.status(404).json({
+      message: "No Project was found!",
+    });
+  }
+
+  res.json({
+    message: "Done",
+    results,
+  });
+});
 
 const getProjectPerformance = catchAsync(async (req, res, next) => {
   let totalProjects = await projectModel.countDocuments(); // Get the total number of projects
@@ -224,6 +278,231 @@ const getProjectPerformance = catchAsync(async (req, res, next) => {
     results,
   });
 });
+const getProjectPerformanceByUser = catchAsync(async (req, res, next) => {
+  let totalProjects = await projectModel.countDocuments(); // Get the total number of projects
+  let id =new mongoose.Types.ObjectId(req.params.id)
+let check = await userModel.findById(id);
+if (!check) { 
+  return res.status(404).json({ message: "User not found!" });
+}
+  let results = await projectModel.aggregate([
+    {
+      $match: 
+      { members:  id  }
+    },
+    {
+      $addFields: {
+        statusCategory: {
+          $cond: {
+            if: { $in: ["$status", ["delayed", "waiting"]] }, // Combine "delayed" and "waiting" into "behind"
+            then: "behind",
+            else: "$status"
+          }
+        }
+      }
+    },
+    {
+      $group: {
+        _id: "$statusCategory", 
+        count: { $sum: 1 }      
+      }
+    },
+    {
+      $facet: {
+        results: [
+          {
+            $project: {
+              _id: 1,
+              count: 1,
+              percentage: {
+                $multiply: [{ $divide: ["$count", totalProjects] }, 100]
+              }
+            }
+          }
+        ],
+        allStatuses: [
+          {
+            $project: {
+              _id: {
+                $literal: ["working", "completed", "behind"]
+              }
+            }
+          }
+        ]
+      }
+    },
+    {
+      $project: {
+        results: {
+          $concatArrays: [
+            "$results",
+            {
+              $map: {
+                input: {
+                  $setDifference: [{ $arrayElemAt: ["$allStatuses._id", 0] }, "$results._id"]
+                },
+                as: "missingStatus",
+                in: { _id: "$$missingStatus", count: 0, percentage: 0 }
+              }
+            }
+          ]
+        }
+      }
+    },
+    {
+      $unwind: "$results"
+    },
+    {
+      $replaceRoot: { newRoot: "$results" }
+    },
+    {
+      $sort: { count: -1 } // Sort by count, descending
+    }
+  ]);
+  if(!results){
+    return res.status(404).json({
+      message: "No Dashboard was found!",
+    })
+  }
+  res.json({
+    message: "Done",
+    results,
+  });
+});
 
 
-export {  getAllDashboard, updateDashboard,getTopCountries ,getUserRatioPieChart ,getActiveProjects,getProjectPerformance };
+const weeklyActivity = catchAsync(async (req, res, next) => {
+//   let id =new mongoose.Types.ObjectId(req.params.id)
+// let check = await userModel.findById(id);
+// if (!check) { 
+//   return res.status(404).json({ message: "User not found!" });
+// }
+const weeklyActivity = await taskModel.aggregate([
+  {
+    $match: {
+      createdAt: { $gte: new Date(new Date().setDate(new Date().getDate() - 7)) }
+    }
+  },
+  {
+    $group: {
+      _id: {
+        dayOfWeek: { $dayOfWeek: "$createdAt" }, // Group by day of the week
+        taskStatus: "$taskStatus" 
+      },
+      count: { $sum: 1 } 
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.dayOfWeek",
+      statuses: {
+        $push: {
+          taskStatus: "$_id.taskStatus",
+          count: "$count"
+        }
+      }
+    }
+  },
+  {
+    $sort: { _id: 1 } 
+  }
+]);
+
+let results = weeklyActivity.map(item => {
+  const data = {
+    day: item._id, // Day of the week (1 = Sunday, 2 = Monday, etc.)
+    delayed: 0,
+    working: 0,
+    completed: 0
+  };
+  
+  item.statuses.forEach(status => {
+    if (status.taskStatus === 'delayed') data.delayed = status.count;
+    if (status.taskStatus === 'working') data.working = status.count;
+    if (status.taskStatus === 'completed') data.completed = status.count;
+  });
+  
+  return data;
+});
+
+  if(!results){
+    return res.status(404).json({
+      message: "No Dashboard was found!",
+    })
+  }
+  res.json({
+    message: "Done",
+    results,
+  });
+});
+const weeklyActivityByUser = catchAsync(async (req, res, next) => {
+  let id =new mongoose.Types.ObjectId(req.params.id)
+let check = await userModel.findById(id);
+if (!check) { 
+  return res.status(404).json({ message: "User not found!" });
+}
+const weeklyActivity = await taskModel.aggregate([
+  {
+    $match: {
+      $match: 
+      { $and: [{ members:  id  }, {createdAt: { $gte: new Date(new Date().setDate(new Date().getDate() - 7))} }] } 
+      
+    }
+  },
+  {
+    $group: {
+      _id: {
+        dayOfWeek: { $dayOfWeek: "$createdAt" }, // Group by day of the week
+        taskStatus: "$taskStatus" 
+      },
+      count: { $sum: 1 } 
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.dayOfWeek",
+      statuses: {
+        $push: {
+          taskStatus: "$_id.taskStatus",
+          count: "$count"
+        }
+      }
+    }
+  },
+  {
+    $sort: { _id: 1 } 
+  }
+]);
+
+let results = weeklyActivity.map(item => {
+  const data = {
+    day: item._id, // Day of the week (1 = Sunday, 2 = Monday, etc.)
+    delayed: 0,
+    working: 0,
+    completed: 0
+  };
+  
+  item.statuses.forEach(status => {
+    if (status.taskStatus === 'delayed') data.delayed = status.count;
+    if (status.taskStatus === 'working') data.working = status.count;
+    if (status.taskStatus === 'completed') data.completed = status.count;
+  });
+  
+  return data;
+});
+
+  if(!results){
+    return res.status(404).json({
+      message: "No Dashboard was found!",
+    })
+  }
+  res.json({
+    message: "Done",
+    results,
+  });
+});
+
+
+export {  getAllDashboard, updateDashboard,getTopCountries ,getUserRatioPieChart ,getActiveProjects,getProjectPerformance ,getActiveProjectsByUser,getProjectPerformanceByUser,
+  weeklyActivity ,weeklyActivityByUser
+ };
--- src/modules/Dashboard/dashboard.routes.js
+++ src/modules/Dashboard/dashboard.routes.js
@@ -5,9 +5,13 @@ import { allowTo, protectRoutes } from "../auth/auth.controller.js";
 const dashboardRouter = express.Router();
 
 dashboardRouter.get("/", dashboardController.getActiveProjects);
+dashboardRouter.get("/user/:id", dashboardController.getActiveProjectsByUser);
 dashboardRouter.get("/performance/", dashboardController.getProjectPerformance);
+dashboardRouter.get("/performance/user/:id", dashboardController.getProjectPerformanceByUser);
 dashboardRouter.get("/country/", dashboardController.getTopCountries);
+dashboardRouter.get("/weekly/", dashboardController.weeklyActivity);
+dashboardRouter.get("/weekly/user/:id", dashboardController.weeklyActivityByUser);
 dashboardRouter.get("/ratio/",protectRoutes,allowTo("admin"), dashboardController.getUserRatioPieChart);
-dashboardRouter.put("/:id", dashboardController.updateDashboard);
+// dashboardRouter.put("/:id", dashboardController.updateDashboard);
 
 export default dashboardRouter;
