4b3e87c7b0d977deac7be6b9e50b7de0dc7c9755
Ahmed Ali <a7med3li0111@gmail.com>
2025-08-12 17:45:27 +0300

    handle mm

--- src/review/review.controller.js
+++ src/review/review.controller.js
@@ -47,11 +47,41 @@ export const getReviews = catchAsync(async (req, res, next) => {
 
   res.status(200).json({ message: "Reviews fetched successfully", data });
 });
+export const getReviewsForAdmin = catchAsync(async (req, res, next) => {
+  const data = await reviewModel.aggregate([
+    {
+      $lookup: {
+        from: "users", // Collection name of the referenced model (check the actual name in MongoDB, usually plural and lowercase)
+        localField: "createdBy",
+        foreignField: "_id",
+        as: "createdBy",
+      },
+    },
+    {
+      $unwind: "$createdBy", // Converts the array to an object (like populate does)
+    },
+    {
+      $project: {
+        _id: 1,
+        text: 1, // Adjust according to your schema
+        rating: 1, // Adjust as needed
+        activation: 1,
+        createdAt: 1,
+        "createdBy._id": 1,
+        "createdBy.name": 1,
+        "createdBy.profilePic": 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({ message: "Reviews fetched successfully", data });
+});
 export const manageReview = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const { status } = req.body;
-  
-  if (status == "undefined") return res.status(400).json({ message: "status is required" });
+
+  if (status == "undefined")
+    return res.status(400).json({ message: "status is required" });
 
   const reviewExist = await reviewModel.findById(id);
   if (!reviewExist) return res.status(404).json({ message: "testo not found" });
